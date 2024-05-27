e624eae5feed97ae1e26249989a8d3495143975e
Felipe Maciel <felipe.maciel2.fm@gmail.com>
2024-05-27 20:29:06 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,134 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as moment from 'moment';
+import "../src/CalendarEvent.css"
+import { Modal } from '@fluentui/react';
+import { Calendar, momentLocalizer } from "react-big-calendar";
+import "react-big-calendar/lib/css/react-big-calendar.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+moment.locale('pt-BR');
+const localizer = momentLocalizer(moment);
+
+
+interface ICalendarItem {
+  name: string;
+  date: Date;
+  localName: string;
 }
 
-export default App;
+interface CalendarEvent {
+  title: string;
+  start: Date;
+  end: Date;
+  resource: ICalendarItem;
+}
+
+export default function App(): JSX.Element {
+  const monthsNames: string[] = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'];
+  const weeklyDay: string[] = ['dom', 'seg', 'ter', 'qua', 'qui', 'sex', 'sab']
+
+
+  const [events, setEvents] = React.useState<ICalendarItem[]>([]);
+  const [showDialog, setShowDialog] = React.useState<boolean>(false);
+  const [selectedEvent, setSelectedEvent] = React.useState<ICalendarItem>();
+
+  React.useEffect(() => {
+    fetch('https://date.nager.at/api/v3/publicholidays/2024/BR')
+      .then((response) => response.json())
+      .then((data) => {
+        const fetchedEvents = Array.isArray(data) ? data.map((item: any) => {
+          const date = new Date(item.date);
+          date.setHours(date.getHours() + (date.getTimezoneOffset() / 60));
+          return {
+            name: item.localName,
+            date: date,
+            localName: item.localName,
+          };
+        }) : [];
+        setEvents(fetchedEvents);
+      })
+      .catch((error) => console.log('Erro ao buscar eventos', error));
+  }, []);
+
+  const calendarStyle = (date: Date) => {
+    const haveEvents = events.some((item) => (
+      item.date.getDate() === date.getDate() &&
+      item.date.getMonth() === date.getMonth() &&
+      item.date.getFullYear() === date.getFullYear()
+    ));
+  
+    if (haveEvents) {
+      return {
+        className: 'calendar-bg-day-have-event'
+      };
+    }
+    return {}; 
+  };
+
+  const onSelectEvent = (event: CalendarEvent): void => {
+    setSelectedEvent(event.resource);
+    setShowDialog(true);
+  };
+  const messages = {
+    allDay: 'Dia Inteiro',
+    previous: '<',
+    next: '>',
+    today: 'Hoje',
+    month: 'Mês',
+    week: 'Semana',
+    day: 'Dia',
+    agenda: 'Agenda',
+    date: 'Data',
+    time: 'Hora',
+    event: 'Evento',
+    showMore: (total:number) => `+${total} Ver mais`
+  }
+  const calendarEvents: CalendarEvent[] = events.map(event => ({
+    title: event.name,
+    start: event.date,
+    end: event.date,
+    resource: event,
+  }));
+
+  return (
+    <>
+      {events.length > 0 && (
+        <>
+          <Calendar
+            localizer={localizer}
+            messages={messages}
+            events={calendarEvents}
+            dayPropGetter={calendarStyle}
+            popup={true}
+            onSelectEvent={onSelectEvent}
+            style={{ height: 500, margin: "50px" }} 
+          />
+          <Modal
+            isOpen={showDialog}
+            onDismiss={() => setShowDialog(false)}
+            className='modal-event-calendar-detail page-calendar'
+          >
+            <div className='event-item-modal-container page-calendar'>
+              <div className="event-item-month page-calendar">
+                {selectedEvent?.date ? monthsNames[new Date(selectedEvent.date).getMonth()] : ''}
+              </div>
+              <div className={`calendar-item page-calendar ${selectedEvent?.date && new Date(selectedEvent.date).getDate() === new Date().getDate() ? 'active' : ''}`}>
+                <div className="calendar-item-date page-calendar">
+                  <div className="calendar-item-date-number page-calendar">
+                    {selectedEvent?.date && new Date(selectedEvent.date).getDate() >= 10 
+                      ? new Date(selectedEvent.date).getDate() 
+                      : `0${selectedEvent?.date ? new Date(selectedEvent.date).getDate() : ''}`}
+                  </div>
+                  <div className="calendar-item-date-weekly page-calendar">
+                    {selectedEvent?.date ? weeklyDay[new Date(selectedEvent.date).getDay()] : ''}
+                  </div>                  
+                </div>
+                <div className="calendar-item-title page-calendar">
+                {selectedEvent?.name}</div>
+              </div>
+            </div>         
+          </Modal>
+        </>
+      )}
+    </>
+  );
+}
